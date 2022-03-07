5e2ac6478d570d49243f20014a8b83dd3bd696e5
Nathan <nathan.e.barron@gmail.com>
2022-03-06 18:09:37 -0600

    Add simple app_name test

--- app/src/androidTest/java/com/example/todo_client/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/todo_client/ExampleInstrumentedTest.kt
@@ -1,24 +1,34 @@
 package com.example.todo_client
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.todo_client.activities.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class NoteInstrumentedTest {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.todo_client", appContext.packageName)
+    fun app_name() {
+        onView(withText(R.string.app_name))
+            .check(matches(isDisplayed()))
     }
+
+    @Test
+    fun scroll_to_note() {
+        // TODO implement to scroll to note
+    }
+
 }
